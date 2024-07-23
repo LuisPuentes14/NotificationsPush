ae43aeb6d338978f0dd71520633074041c0562e1
AlejandroPuentes <alejandropuentes@wposs.com>
2024-07-23 10:41:16 -0500

    se agregan pruebas unitarias con mock's

---
+++ NotificationsPush.TestMoq/Controllers/AuthorizationControllerTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NotificationsPush.Controllers;
+using NotificationsPush.DTO.Request;
+using NotificationsPush.DTO.Response;
+using NotificationsPush.Models.Local;
+using NotificationsPush.Services.Interfaces;
+
+namespace NotificationsPush.Tests.Controllers
+{
+    public class AuthorizationControllerTest
+    {
+
+        private readonly Mock<IAuthenticationService> _mockAuthenticationService;
+        private readonly AuthorizationController _controller;
+
+        public AuthorizationControllerTest() {
+            _mockAuthenticationService = new Mock<IAuthenticationService>();
+            _controller = new AuthorizationController(_mockAuthenticationService.Object);
+        }
+
+        [Fact]
+        public async void AuthenticationTest()
+        {
+            // Arrange
+
+            User user = new User();
+            user.user = "alejandro";
+            user.password = "dsf3w345r43gfdgdfg";
+            user.type = "USER";
+
+            UserRequest request = new UserRequest();
+            request.user = user.user;
+            request.password = user.password;
+            request.type = user.type;
+
+
+            UserAuthenticated userAuthenticated = new UserAuthenticated();
+            userAuthenticated.status = true;
+            userAuthenticated.message = "proceso exito.";
+            userAuthenticated.token = "token";
+            userAuthenticated.minutesExpiresToken = "560";
+
+            _mockAuthenticationService.Setup(service => service.Authentication(It.Is<User>(u =>
+                u.user == user.user &&
+                u.password == user.password &&
+                u.type == user.type))).ReturnsAsync(userAuthenticated);
+
+
+            // Act
+            var result = await _controller.Authentication(request);
+
+
+            // Assert
+            Assert.NotNull(result);
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var genericResponse = Assert.IsType<GenericResponse<object>>(okResult.Value);
+
+            Assert.Equal(userAuthenticated.status, genericResponse.status);
+            Assert.Equal(userAuthenticated.message, genericResponse.message);
+        }
+    }
+}
---
+++ NotificationsPush.TestMoq/Controllers/NotificationControllerTest.cs
@@ -0,0 +1,73 @@
+﻿using Moq;
+using NotificationsPush.Controllers;
+using NotificationsPush.Models.Local;
+using NotificationsPush.Services.Interfaces;
+
+
+namespace NotificationsPush.Tests.Controllers
+{
+    public class NotificationControllerTest
+    {
+        private readonly Mock<INotificationsService> _mockAuthenticationService;
+        private readonly NotificationController _controller;
+
+        public NotificationControllerTest()
+        {
+            _mockAuthenticationService = new Mock<INotificationsService>();
+            _controller = new NotificationController(_mockAuthenticationService.Object);
+        }
+
+        [Fact]
+        public async void GetNotitificationsPendingTest()
+        {
+            // Arrange
+
+            List<NotificationPending> notificationPendings = new List<NotificationPending>() {
+                new NotificationPending (){
+                    icon = "icono en base64",
+                    picture = "imagen en base 63",
+                    description = "descripcion de prueba",
+                    title= "titulo de prueba" }
+
+            };
+
+            _mockAuthenticationService.Setup(Services => Services.GetNotitificationsPending("123456790")).ReturnsAsync(notificationPendings);
+
+            // Act
+            var result = _controller.GetNotitificationsPending("123456790");
+
+            // Assert
+            Assert.NotNull(result);
+
+        }
+
+        [Fact]
+        public async void SendNotificationTest()
+        {
+            // Arrange
+
+            SentTerminalsStatus sentTerminalsStatus = new SentTerminalsStatus();
+            sentTerminalsStatus.terminalNotSend = new List<string>() { "1234567890" };
+            sentTerminalsStatus.terminalSend = new List<string>() { "0987654321" };
+
+            SendNotification sendNotification = new SendNotification();
+
+            sendNotification.terminal_serial = new List<string>() { "1234567890", "0987654321" };
+            sendNotification.icon = "ICOCNO BASE64";
+            sendNotification.picture = "IMAGEN BASE 64";
+            sendNotification.title = "TITULO DE PRUEBA";
+            sendNotification.description = "DECRIPCION DE PRUEBA";
+            sendNotification.notification_id = 0;
+
+            _mockAuthenticationService.Setup(Services => Services.SendNotification(sendNotification)).ReturnsAsync(sentTerminalsStatus);
+
+            // Act
+            var result = _controller.GetNotitificationsPending("123456790");
+
+            // Assert
+            Assert.NotNull(result);
+
+        }
+
+    }
+}
